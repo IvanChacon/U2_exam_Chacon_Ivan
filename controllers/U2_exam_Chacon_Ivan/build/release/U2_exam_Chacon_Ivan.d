bbca130c951d3878f0066f1df0a2ab9327555e91
ivanupy <al1701019@upy.edu.mx>
2019-06-27 12:26:55 -0500

    First commit, Robot1 moves forward and stops when the sensor detects an object at a distance of 15cm

---
+++ controllers/U2_exam_Chacon_Ivan/build/release/U2_exam_Chacon_Ivan.d
@@ -0,0 +1,7 @@
+build/release/U2_exam_Chacon_Ivan.o: U2_exam_Chacon_Ivan.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/keyboard.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h
