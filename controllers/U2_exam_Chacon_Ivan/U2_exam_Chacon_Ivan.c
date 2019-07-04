11e7130486756ebd659e3e7e62084638b47e3bd0
ivanupy <al1701019@upy.edu.mx>
2019-07-04 01:19:11 -0500

    Some corrections on robot 1 were made, when it detects obstacles turns 90 degrees to the right side

--- controllers/U2_exam_Chacon_Ivan/U2_exam_Chacon_Ivan.c
+++ controllers/U2_exam_Chacon_Ivan/U2_exam_Chacon_Ivan.c
@@ -10,117 +10,141 @@
  * You may need to add include files like <webots/distance_sensor.h> or
  * <webots/differential_wheels.h>, etc.
  */
- #include <webots/robot.h>
- #include <webots/motor.h>
- #include <webots/keyboard.h>
- #include <webots/distance_sensor.h>
+#include <webots/robot.h>
+#include <webots/motor.h>
+#include <webots/position_sensor.h>
+#include <webots/distance_sensor.h>
 
- #include <stdio.h>
- #include <math.h>
- /*
- enum {
-  WB_KEYBOARD_END,
-  WB_KEYBOARD_HOME,
-  WB_KEYBOARD_LEFT,
-  WB_KEYBOARD_UP,
-  WB_KEYBOARD_RIGHT,
-  WB_KEYBOARD_DOWN,
-  WB_KEYBOARD_PAGEUP,
-  WB_KEYBOARD_PAGEDOWN,
-  WB_KEYBOARD_NUMPAD_HOME,
-  WB_KEYBOARD_WB_KEYBOARD_PAGEDOWN,
-  WB_KEYBOARD_NUMPAD_UP,
-  WB_KEYBOARD_NUMPAD_RIGHT,
-  WB_KEYBOARD_NUMPAD_DOWN,
-  WB_KEYBOARD_NUMPAD_END,
-  WB_KEYBOARD_KEY,
-  WB_KEYBOARD_SHIFT,
-  WB_KEYBOARD_CONTROL,
-  WB_KEYBOARD_ALT
- };
-  */
+#include <stdio.h>
+#include <math.h>
 
- /*
-  * You may want to add macros here.
-  */
- #define TIME_STEP 64
-
- /*
-  * This is the main program.
-  * The arguments of the main function can be specified by the
-  * "controllerArgs" field of the Robot node
-  */
- int main(int argc, char **argv)
- {
-   /* necessary to initialize webots stuff */
-   wb_robot_init();
-   wb_keyboard_enable(TIME_STEP); //sample period for the keyboard, the same as
-                                 // TIME_STEP, to use our real keyboard on wb
-   //int pressed_key;
+/*
+ * You may want to add macros here.
+ */
+#define TIME_STEP 64
+#define PI 3.14159
+#define OBSTACLE_DISTANCE 35
 
 
-   /*
-    * You should declare here WbDeviceTag variables for storing
-    * robot devices like this:
-    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-    */
-    // Motor devices
-    WbDeviceTag wheel_right = wb_robot_get_device("motor_right");
-    WbDeviceTag wheel_left = wb_robot_get_device("motor_left");
+enum {
+  GO,
+  TURN,
+  FREEWAY,
+  OBSTACLE
+  };
 
-    wb_motor_set_position (wheel_right, INFINITY);
-    wb_motor_set_position (wheel_left, INFINITY);
+  //Global variables
 
-    // Distance sensor devices
-    WbDeviceTag dist_sensor  = wb_robot_get_device("distance_sensor");
+  double initial_angle_wheel1;
 
-    wb_distance_sensor_enable (dist_sensor, TIME_STEP);
+/*
+ * Functions
+ */
 
-    double ds_value;
+    void goRobot (WbDeviceTag *wheels, float velocity) {
+       wb_motor_set_velocity(wheels[0], velocity);
+       wb_motor_set_velocity(wheels[1], velocity);
+    }
 
-   /* main loop
-    * Perform simulation steps of TIME_STEP milliseconds
-    * and leave the loop when the simulation is over
-    */
-   while (wb_robot_step(TIME_STEP) != -1) {
+    void stopRobot (WbDeviceTag *wheels, float noVel) {
+       wb_motor_set_velocity(wheels[0], noVel);
+       wb_motor_set_velocity(wheels[1], noVel);
+    }
 
-     /*
-      * Read the sensors :
-      * Enter here functions to read sensor data, like:
-      *  double val = wb_distance_sensor_get_value(my_sensor);
-      */
-    //  pressed_key= wb_keyboard_get_key();
+     int LookForObstacles (WbDeviceTag dist_sensor) {
+       double distance = wb_distance_sensor_get_value(dist_sensor);
+       printf("Distance: %lf\n", distance);
 
+       if (distance > OBSTACLE_DISTANCE)
+ 	 return FREEWAY;
+ 	   else
+ 	     return OBSTACLE;
+     }
 
-      ds_value = wb_distance_sensor_get_value (dist_sensor);
-      printf("Distance sensor: %lf\n", ds_value );
+      void turn90right (WbDeviceTag *wheels, float velocity) {
+          wb_motor_set_velocity(wheels[0], velocity);
+          wb_motor_set_velocity(wheels[1], -velocity);
+      }
 
+     double getAngleRobot(WbDeviceTag encoder1) {
+       printf("Angle\n");
+       double angle, angle_wheel1;
 
-     /* Process sensor data here */
+       angle_wheel1 = wb_position_sensor_get_value(encoder1);
+       printf("Angle wheel1: %lf\n", angle_wheel1);
+       angle = fabs(angle_wheel1 - initial_angle_wheel1);
+       printf("Angle: %lf\n", angle);
 
-     /*
-      * Enter here functions to send actuator commands, like:
-      * wb_differential_wheels_set_speed(100.0,100.0);
-      */
+       return angle;
+     }
 
+int main(int argc, char **argv)
+{
+  /* necessary to initialize webots stuff */
+  wb_robot_init();
 
+  /*
+   * You should declare here WbDeviceTag variables for storing
+   * robot devices like this:
+   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
+   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
+   */
+   // Motor devices
+      WbDeviceTag wheels[2];
+   wheels[0] = wb_robot_get_device("motor_right");
+   wheels[1] = wb_robot_get_device("motor_left");
 
-         wb_motor_set_velocity(wheel_right, -6.66);
-          wb_motor_set_velocity(wheel_left, -6.66);
+   wb_motor_set_position (wheels[0], INFINITY);
+   wb_motor_set_position (wheels[1], INFINITY);
 
-       if(ds_value <= 20){
-         wb_motor_set_velocity(wheel_right, 0);
-          wb_motor_set_velocity(wheel_left, 0);
+   // Encoder devices
+   WbDeviceTag encoder_right = wb_robot_get_device("encoder1");
+   wb_position_sensor_enable(encoder_right, TIME_STEP);
+   WbDeviceTag encoder_left = wb_robot_get_device("encoder2");
+   wb_position_sensor_enable(encoder_left, TIME_STEP);
 
-       }
+   // Distance sensor devices
+   WbDeviceTag dist_sensor  = wb_robot_get_device("distance_sensor");
+   wb_distance_sensor_enable (dist_sensor, TIME_STEP);
 
-   };
+   //variables
+   float velocity = -6.333;
+   float noVel = 0;
+   float angle;
+   int ds_state, robot_state = GO;
 
-   /* Enter your cleanup code here */
+  /* main loop
+   * Perform simulation steps of TIME_STEP milliseconds
+   * and leave the loop when the simulation is over
+   */
+  while (wb_robot_step(TIME_STEP) != -1) {
+    if (robot_state == GO) {
+      ds_state = LookForObstacles(dist_sensor);
 
-   /* This is necessary to cleanup webots resources */
-   wb_robot_cleanup();
+      if (ds_state == FREEWAY) {
+        goRobot(wheels, velocity);
+        angle = wb_position_sensor_get_value(encoder_right);
+        printf("Angle: %lf\n", angle);
+      } else if (ds_state == OBSTACLE) {
+        robot_state = TURN;
+        stopRobot(wheels, noVel);
+        initial_angle_wheel1 = wb_position_sensor_get_value(encoder_right);
+      }
+    } else if (robot_state == TURN) {
+      turn90right(wheels, velocity);
+      angle = getAngleRobot(encoder_right);
 
-   return 0;
- }
+      if (angle >= 0.59*PI) {
+        robot_state = GO;
+        stopRobot(wheels, noVel);
+      }
+    }
+	}
+
+  /* Enter your cleanup code here */
+
+  /* This is necessary to cleanup webots resources */
+  wb_robot_cleanup();
+
+  return 0;
+}
--- controllers/U2_exam_Chacon_Ivan/build/release/U2_exam_Chacon_Ivan.d
+++ controllers/U2_exam_Chacon_Ivan/build/release/U2_exam_Chacon_Ivan.d
@@ -3,5 +3,5 @@ build/release/U2_exam_Chacon_Ivan.o: U2_exam_Chacon_Ivan.c \
  /usr/local/webots/include/controller/c/webots/types.h \
  /usr/local/webots/include/controller/c/webots/nodes.h \
  /usr/local/webots/include/controller/c/webots/motor.h \
- /usr/local/webots/include/controller/c/webots/keyboard.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h \
  /usr/local/webots/include/controller/c/webots/distance_sensor.h
